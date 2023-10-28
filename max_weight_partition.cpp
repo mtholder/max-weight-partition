a7a0256ffdeee456bb8334d5ed13a1a682174743
Mark T. Holder <mtholder@gmail.com>
2023-10-28 15:07:50 -0500

    start of parser

--- max_weight_partition.cpp
+++ max_weight_partition.cpp
@@ -4,9 +4,33 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <list>
+
+#include <stdlib.h>     /* strtol */
 using namespace std;
 using otc::OTCError;
 
+// also from otc
+std::list<std::string> split_string(const std::string &s, const char delimiter);
+
+std::list<std::string> split_string(const std::string &s, const char delimiter) {
+    // FIXME - this preserves old behavior - I don't know if we actually need this
+    if (s.empty()) {
+        return {};
+    }
+    std::list<std::string> r;
+    r.push_back({});
+    for (const auto & c : s) {
+        if (c == delimiter) {
+            r.push_back({});
+        } else {
+            r.back().append(1, c);
+        }
+    }
+    return r;
+}
+
+// end otc
 
 void read_labels(string & fp, vector<string> & idx2name, map<string, size_t> & name2idx) {
     ifstream inp{fp};
@@ -14,10 +38,29 @@ void read_labels(string & fp, vector<string> & idx2name, map<string, size_t> & n
         throw OTCError() << "file \"" << fp << "\" is not a readable file.";
     }
     string next_line;
-    int num_subsets = -1;
+    long num_subsets = -1;
+    long subsets_read = 0;
     while (getline(inp, next_line)) {
-        cout << next_line << endl;
-
+        if (num_subsets < 0) {
+            char * i_end;
+            auto line_c = next_line.c_str();
+            num_subsets = strtol(line_c, &i_end, 10);
+            //double d = strtod(s.c_str(), nullptr);
+            if (num_subsets < 1) {
+                throw OTCError() << "at least on subset is required.";
+            }
+            if ((i_end - line_c) != next_line.size()) {
+                throw OTCError() << "content after the number of subsets is not allowed on line 1";
+            }
+        } else {
+            auto sub_split = split_string(next_line, ',');
+            if (!sub_split.empty()) {
+                subsets_read++;
+            }
+        }
+    }
+    if (subsets_read != num_subsets) {
+        throw OTCError() << "Expecting " << num_subsets << " lines, but read " << subsets_read;
     }
 }
 
