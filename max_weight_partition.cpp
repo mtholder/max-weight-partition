6ae374dfdbaefc13dd5d3269496d09a5adcc9097
Mark T. Holder <mtholder@gmail.com>
2023-10-28 14:47:30 -0500

    example file

--- max_weight_partition.cpp
+++ max_weight_partition.cpp
@@ -2,18 +2,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <vector>
 using namespace std;
 using otc::OTCError;
 
-void run(std::string &fp) {
+
+void read_labels(string & fp, vector<string> & idx2name, map<string, size_t> & name2idx) {
     ifstream inp{fp};
     if (!inp.good()) {
         throw OTCError() << "file \"" << fp << "\" is not a readable file.";
     }
     string next_line;
+    int num_subsets = -1;
     while (getline(inp, next_line)) {
-        cout << next_line;
+        cout << next_line << endl;
+
     }
+}
+
+
+void run(std::string &fp) {
+    vector<string> idx2name;
+    map<string, size_t> name2idx;
+    read_labels(fp, idx2name, name2idx);
 }
 
 int main(int argc, char *argv[]) {
